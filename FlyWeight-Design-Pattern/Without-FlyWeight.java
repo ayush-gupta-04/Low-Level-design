3368d5c67302496e43afd7fc47bbf27a67f665e1
Ayush Gupta <152642477+ayush-gupta-04@users.noreply.github.com>
2026-05-20 20:14:57 +0530

    Create Without-FlyWeight.java

---
+++ FlyWeight-Design-Pattern/Without-FlyWeight.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+
+class FlyWeight{
+    String state;
+    int posX;
+    int posY;
+    public FlyWeight(String state , int posX ,int posY){
+        this.state = state;
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    public void render(int posX, int posY){
+        System.out.println("State : " + state + " posX : " + posX + " posY : " + posY);
+    }
+
+    public static int getMemory(){
+        return Character.BYTES*6 + Integer.BYTES*2;
+    }
+}
+
+
+
+
+class Game {
+    private List<FlyWeight> flyWeights;
+    public Game(){
+        this.flyWeights = new ArrayList<>();
+    }
+
+    public void makeFlyWeights(int cnt){
+        String[] states = {"state0" , "state1" , "state2" , "state3"};
+        for(int i = 0;i < cnt ;i++){
+            int type = i%4;
+            flyWeights.add(new FlyWeight(states[type], (100 + i*2), (100 + i*3)));
+        }
+    }
+
+    public long calcMemory(){
+        long mem = flyWeights.size()*FlyWeight.getMemory();
+        return mem;
+    }
+}
+
+
+public class Main{
+    public static void main(String[] args) {
+        Game g = new Game();
+        g.makeFlyWeights(10000000);
+        System.out.println( "Total Memory : " + g.calcMemory());
+        System.out.println( "Total Memory in MB : " + g.calcMemory()/(1024.0*1024.0));
+    }
+}
+
+
